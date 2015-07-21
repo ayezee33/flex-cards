056f8b3f96d578cc8c7a1625bbcbc1f1586e1cd1
ayezee33 <ayezee@Alexs-MacBook-Pro.local>
2015-07-21 16:21:23 -0700

    Honestly not sure what all these changes are from

---
+++ assets/flex-card.php
@@ -0,0 +1,15 @@
+<div class="flex-card">
+	<div class="hero">
+	    <h3>Material Design Flex Card</h3>
+	</div><!--End of Hero-->
+
+	<div class="content">
+	    <p>This is content. Content is good because it can give context. Unless it is convuluted, in which case it can be rather confusing.</p>
+	</div><!--End of Content-->
+
+	<div class="footer">
+	    <p>Learn More</p>
+			<img src="images/codepen.png">
+			<img src="images/GitHub-Mark.png">
+	</div><!--End Footer-->
+</div><!--End Flex Card-->
--- assets/image-card.php
+++
@@ -1,9 +0,0 @@
-<div class="flex-card">
-    <div class="content-img">
-    	<h3>This is a title</h3>
-    </div><!--End of Content-->
-
-    <div class="footer">
-        <p>This is the footer content</p>
-    </div><!--End Footer-->
-</div><!--End Flex Card-->
